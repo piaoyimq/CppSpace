2312857aed0ad8a1f1ff0ad7819c53662a152490
Weibo Zhu <weibo.zhu@ericsson.com>
2015-11-06 08:38:33 +0100

    Add Function directory
    Change-Id: I39e18783d64785894ae0088fc0e13c18fd57bcd3

---
+++ PiaoyimqGeneralCppCode/cpp-test/explicit.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+class Test1
+{
+    public:
+        Test1(int n)
+        {
+            num=n;
+        }//普通构造函数
+    private:
+        int num;
+};
+
+class Test2
+{
+    public:
+        explicit Test2(int n)
+        {
+            num=n;
+        }//explicit(显式)构造函数
+    private:
+        int num;
+};
+
+class Test3
+{
+    public:
+        explicit Test3(int n, int a=3)
+        {
+            num=n;
+        }//explicit(显式)构造函数
+    private:
+        int num;
+};
+
+int main()
+{
+    Test1 t1=12;//隐式调用其构造函数,成功
+//    Test2 t2=12;//编译错误,不能隐式调用其构造函数
+    Test2 t2(12);//显式调用成功
+    //Test3 t31 = 3;//error 
+    Test3 t3(12,1);//显式调用成功
+    return 0;
+}
