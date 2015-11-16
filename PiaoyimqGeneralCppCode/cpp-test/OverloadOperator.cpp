8bc68d9900adbd6ef2d8d08f2708a984bc7befd7
Weibo Zhu <weibo.zhu@ericsson.com>
2015-11-16 11:06:30 +0100

    Modify OverloadOperator.cpp
    Change-Id: Ib76776ee511519fa9143e2023c3c1f1963dfb461

--- PiaoyimqGeneralCppCode/cpp-test/OverloadOperator.cpp
+++ PiaoyimqGeneralCppCode/cpp-test/OverloadOperator.cpp
@@ -5,149 +5,269 @@
  *      Author: ezhweib
  */
 
-
 #include <iostream>
-#include <vector>
+#include <cstring>
+#include <iomanip>
+#include <cassert>
+#include <utility> //move
+using namespace std;
 
-    template <class AllocT = std::allocator<char> > class String {
-    public :
+class String {
+public:
+    String();
+    String(const char *);
+    String(const String &);
+//    String(String&& str);
 
-        ~String();
+    ~String();
 
-        explicit String(const std::string& str, const AllocT& allocator = AllocT());
+    String& operator=(const char *);
+    String& operator=(const String &);
 
-        void print(std::ostream& os) const;
+    bool operator==(const char *);
+    bool operator==(const String &);
 
-        char& operator[](std::size_t pos);
+    char &operator[](int);
 
-        const char& operator[](std::size_t pos) const;
+    String operator+(const char *);
+    String operator+(const char);
+    String operator+(const String &);
 
-        std::size_t size() const;
+    String &operator +=(const char *);
+    String &operator +=(const char);
+    String &operator +=(const String &);
 
-        String<AllocT>* operator=(const std::string& rhs);
-
-#if 1//zhuweibo operator
-        String&
-             operator+=(const String& __str)
-             { }
-
-        basic_string&
-             operator=(const basic_string& __str)
-             { return this->assign(__str); }
-
-        template<typename _CharT, typename _Traits, typename _Alloc>
-            basic_string<_CharT, _Traits, _Alloc>
-            operator+(const basic_string<_CharT, _Traits, _Alloc>& __lhs,
-                  const basic_string<_CharT, _Traits, _Alloc>& __rhs)
-            {
-              basic_string<_CharT, _Traits, _Alloc> __str(__lhs);
-              __str.append(__rhs);
-              return __str;
-            }
-
-        template<typename _CharT, typename _Traits, typename _Alloc>
-            inline bool
-            operator==(const basic_string<_CharT, _Traits, _Alloc>& __lhs,
-                   const basic_string<_CharT, _Traits, _Alloc>& __rhs)
-            { return __lhs.compare(__rhs) == 0; }
-
-        template<typename _CharT, typename _Traits, typename _Alloc>
-            inline bool
-            operator!=(const basic_string<_CharT, _Traits, _Alloc>& __lhs,
-                   const basic_string<_CharT, _Traits, _Alloc>& __rhs)
-            { return !(__lhs == __rhs); }
-
-        template<typename _CharT, typename _Traits, typename _Alloc>
-            inline bool
-            operator<(const basic_string<_CharT, _Traits, _Alloc>& __lhs,
-                  const basic_string<_CharT, _Traits, _Alloc>& __rhs)
-            { return __lhs.compare(__rhs) < 0; }
-
-        template<typename _CharT, typename _Traits, typename _Alloc>
-            inline bool
-            operator<(const _CharT* __lhs,
-                  const basic_string<_CharT, _Traits, _Alloc>& __rhs)
-            { return __rhs.compare(__lhs) > 0; }
-
-        template<typename _CharT, typename _Traits, typename _Alloc>
-            inline bool
-            operator<=(const basic_string<_CharT, _Traits, _Alloc>& __lhs,
-                   const basic_string<_CharT, _Traits, _Alloc>& __rhs)
-            { return __lhs.compare(__rhs) <= 0; }
-
-        template<typename _CharT, typename _Traits, typename _Alloc>
-           basic_istream<_CharT, _Traits>&
-           operator>>(basic_istream<_CharT, _Traits>& __is,
-                  basic_string<_CharT, _Traits, _Alloc>& __str);
-
-#endif
-
-        explicit String(const AllocT& allocator = AllocT());
-
-        template <class AllocU> inline String<AllocT>* operator=(const String<AllocU>& rhs) {
-            itsVector = rhs.itsVector;
-        }
-
-        void append(const std::string& str);
-
-        void resize(std::size_t n);
-
-
-    private :
-
-        std::vector<char,AllocT> itsVector;
-    };
-
-
-    template <class AllocT> String<AllocT>::String(const std::string& str, const AllocT& allocator) : itsVector(str.c_str(), str.c_str() + str.size() + 1, allocator) {
+    int size() {
+        return _size;
+    }
+    char *c_str() {
+        return _string;
     }
 
-    template <class AllocT> void String<AllocT>::print(std::ostream& os) const {
-        os << &itsVector[0];
-    }
+    friend istream &operator>>(istream &cin, String &str);
+private:
+    int _size;
+    char *_string;
+};
 
-    template <class AllocT> char& String<AllocT>::operator[](std::size_t pos) {
-        return itsVector[pos];
-    }
+String::String() {
+    _size = 0;
+    _string = NULL;
 
-    template <class AllocT> const char& String<AllocT>::operator[](std::size_t pos) const {
-        return itsVector[pos];
-    }
-
-    template <class AllocT> std::size_t String<AllocT>::size() const {
-        if (!itsVector.empty())
-        {
-            return strlen(&itsVector[0]);
-        }
-        return 0;
-    }
-
-    template <class AllocT> String<AllocT>* String<AllocT>::operator=(const std::string& rhs) {
-        itsVector.resize(rhs.size() + 1);
-        memcpy(&itsVector[0], rhs.c_str(), rhs.size());
-        itsVector[rhs.size()] = '\0';
-    }
-
-    template <class AllocT> String<AllocT>::String(const AllocT& allocator) : itsVector(allocator) {
-    }
-
-    template <class AllocT> void String<AllocT>::append(const std::string& str) {
-        const std::size_t end = size();
-        itsVector.resize(size() + str.size() + 1);
-        memcpy(&itsVector[end], str.c_str(), str.size() + 1);
-    }
-
-    template <class AllocT> void String<AllocT>::resize(std::size_t n) {
-        const std::size_t len = size();
-        itsVector.resize(n);
-        itsVector[len] = '\0';
-    }
-
-    template <class AllocT> String<AllocT>::~String() {
-    }
-
-
-int main(){
-    std::string s;
-
+    cout << "String() call" << endl;
 }
+
+String::String(const char *str) {
+    if (!str) {
+        _size = 0;
+        _string = NULL;
+    } else {
+        _size = strlen(str);
+        _string = new char[_size + 1];
+        strcpy(_string, str);
+        _string[_size] = 0;
+    }
+
+    cout << "String(const char *str) call" << endl;
+}
+
+//String::String(String && str) {
+//    _size = str._size;
+//    _string = str._string;
+//    str._string = NULL;
+//}
+
+String::String(const String &str) {
+    if (!str._size) {
+        _size = 0;
+        _string = NULL;
+    } else {
+        _size = str._size;
+        _string = new char[_size + 1];
+        strcpy(_string, str._string);
+        _string[_size] = 0;
+    }
+
+    cout << "String(const String &str) call" << endl;
+}
+
+String::~String() {
+    if (_string)
+        delete _string;
+
+    cout << "~String() call" << endl;
+}
+
+String &String::operator=(const char *str) {
+    //Èç¹ûstrÓëµ±Ç°×Ö·û´®ÊÇÏàÍ¬µÄ
+    if (_string == str)
+        return *this;
+    if (!str) {
+        _size = 0;
+        if (_string) {
+            delete[] _string;
+            _string = NULL;
+        }
+    } else {
+        _size = strlen(str);
+        if (_string)
+            delete[] _string;
+        _string = new char[_size + 1];
+        strcpy(_string, str);
+        _string[_size] = 0;
+    }
+    return *this;
+}
+
+String &String::operator=(const String &str) {
+    cout << "operator =(const String &str) call" << endl;
+    if (_string == str._string)
+        return *this;
+    if (!str._size) {
+        _size = 0;
+        if (_string) {
+            delete[] _string;
+            _string = NULL;
+        }
+    } else {
+        _size = str._size;
+        if (_string)
+            delete[] _string;
+        _string = new char[_size + 1];
+        strcpy(_string, str._string);
+        _string[_size] = 0;
+    }
+    return *this;
+}
+
+bool String::operator==(const char *str) {
+    if (strcmp(str, _string) == 0)
+        return true;
+    return false;
+}
+
+bool String::operator ==(const String &str) {
+    if (strcmp(str._string, _string) == 0)
+        return true;
+    return false;
+}
+
+char &String::operator[](int index) {
+    assert(index >= 0 && index < _size);
+    return _string[index];
+}
+
+String String::operator+(const char *str) {
+    assert(_string && str);
+
+    String str_temp(*this);
+
+    str_temp += str;
+    return std::move(str_temp);
+}
+
+String String::operator+(const char x) {
+    assert(_string);
+
+    String str_temp(*this);
+
+    str_temp += x;
+    return std::move(str_temp);
+}
+
+String String::operator+(const String &str) {
+    assert(_string && str._string);
+
+    String str_temp(*this);
+
+    str_temp += str;
+    return std::move(str_temp);
+}
+
+String &String::operator+=(const char x) {
+    assert(_string);
+
+    char *temp_string = new char[_size + 2];
+    strcpy(temp_string, _string);
+    temp_string[_size] = x;
+    _size += 1;
+    temp_string[_size] = 0;
+    delete[] _string;
+    _string = temp_string;
+    return *this;
+}
+
+String &String::operator+=(const char *str) {
+    assert(_string && str);
+
+    int d_size = strlen(str);
+    char *temp_string = new char[_size + d_size + 1];
+    strcpy(temp_string, _string);
+    strcpy(temp_string + _size, str);
+    _size += d_size;
+    temp_string[_size] = 0;
+    delete[] _string;
+    _string = temp_string;
+    return *this;
+}
+
+String &String::operator+=(const String &str) {
+    assert(_string && str._string);
+
+    int d_size = str._size;
+    char *temp_string = new char[_size + d_size + 1];
+    strcpy(temp_string, _string);
+    strcpy(temp_string + _size, str._string);
+    _size += d_size;
+    temp_string[_size] = 0;
+    delete[] _string;
+    _string = temp_string;
+    return *this;
+}
+
+istream &operator>>(istream &cin, String &str) {
+    const int limit_string_size = 4096;
+
+    str._string = new char[limit_string_size];
+
+    cin >> setw(limit_string_size) >> str._string;
+    str._size = strlen(str._string);
+    return cin;
+}
+
+ostream &operator<<(ostream &cout, String &str) {
+    return cout << str.c_str();
+}
+
+int main(int argc, char *argv[]) {
+    //int ia[7] = {0, 1, 1, 2, 3, 5, 8};
+
+    ////vector<int> ivec = {0, 1, 1, 2, 3, 5, 8}; ³ö´í
+    //vector<int> ivec(ia, ia + 7);
+    ////vector<string> svec = ivec;
+    //vector<string> svec(10, string("null"));
+    //for (int i = 0; i < svec.size(); i ++)
+    //  cout << svec[i] << endl;
+
+    String str1("Huang"), str2("Chen");
+
+    cin >> str1;
+    str1 = str1 + 'c' + "CC";
+    str1 += 'a';
+    cout << str1 << " " << str2 << endl;
+    cout << (str2 == "Chen") << endl;
+    return 0;
+}
+
+//String(const char *str)  call
+//String(const char *str)  call
+//String() call
+//String(const String &str)  call
+//~String()
+//String() call
+//String(const String &str)  call
+//~String()
+//~String()
+//~String()
+//~String()
+//~String()
