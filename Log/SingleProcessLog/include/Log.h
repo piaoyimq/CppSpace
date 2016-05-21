15da0e441c05a4b6059f54e115b77087af5e68cf
piaoyimq <piaoyimq@qq.com>
2016-05-21 17:15:19 +0800

    SingleProcessLog: format code

--- Log/SingleProcessLog/include/Log.h
+++ Log/SingleProcessLog/include/Log.h
@@ -39,151 +39,167 @@ using namespace std;
 #define LOG_DIRECTORY   ".." //"/var/log/"
 
 #if 1//TODO: would be move to common directory. (piaoyimq)
-inline static pid_t getTid() {   // Must a static function, it called by a static funcion flushLogThread().
-    return syscall(SYS_gettid);
+inline static pid_t getTid()
+{   // Must a static function, it called by a static funcion flushLogThread().
+	return syscall(SYS_gettid);
 }
 
-inline void getNameByPid(pid_t pid, char *processName) {    //should become a un inline function after move to common.
-    char procPidPath[DIR_LENGTH + 1] = { '\0' };
-    char buf[BUF_SIZE] = { '\0' };
-    sprintf(procPidPath, "/proc/%d/status", pid);
-    FILE* fp = fopen(procPidPath, "r");
-    if (NULL != fp) {
-        if (fgets(buf, BUF_SIZE - 1, fp) == NULL) {
-            fclose(fp);
-        }
-        fclose(fp);
-        sscanf(buf, "%*s %s", processName);
-    }
+inline void getNameByPid(pid_t pid, char *processName)
+{    //should become a un inline function after move to common.
+	char procPidPath[DIR_LENGTH + 1] = { '\0' };
+	char buf[BUF_SIZE] = { '\0' };
+	sprintf(procPidPath, "/proc/%d/status", pid);
+	FILE* fp = fopen(procPidPath, "r");
+	if (NULL != fp)
+	{
+		if (fgets(buf, BUF_SIZE - 1, fp) == NULL)
+		{
+			fclose(fp);
+		}
+		fclose(fp);
+		sscanf(buf, "%*s %s", processName);
+	}
 }
 
-inline bool isDirPathExist(const char* dirpPath) {
-    if (NULL == dirpPath || NULL == opendir(dirpPath)) {
-        return false;
-    }
-    return true;
+inline bool isDirPathExist(const char* dirpPath)
+{
+	if (NULL == dirpPath || NULL == opendir(dirpPath))
+	{
+		return false;
+	}
+	return true;
 }
 #endif
 
 template<class T>
-inline size_t getArrayLen(T& array) {
-    return (sizeof(array) / sizeof(array[0]));
+inline size_t getArrayLen(T& array)
+{
+	return (sizeof(array) / sizeof(array[0]));
 }
 
-enum AppModuleId {
-    LogId = 0, ConfId, LastId
+enum AppModuleId
+{
+	LogId = 0, ConfId, LastId
 };
 
 const char logModuleString[][STRING_LENGTH] = { "Log", "Conf" };
 
-enum Level {
-    Emergency = 0, /* system is unusable */
-    Alert, /* action must be taken immediately */
-    Critical, /* critical conditions */
-    Error, /* error conditions */
-    Warning, /* warning conditions */
-    Notice, /* normal but significant condition */
-    Info, /* informational */
-    Debug, /* debug-level messages */
-    LastLevel
+enum Level
+{
+	Emergency = 0, /* system is unusable */
+	Alert, /* action must be taken immediately */
+	Critical, /* critical conditions */
+	Error, /* error conditions */
+	Warning, /* warning conditions */
+	Notice, /* normal but significant condition */
+	Info, /* informational */
+	Debug, /* debug-level messages */
+	LastLevel
 };
 
 const char logLevelString[][STRING_LENGTH] = { "EMERG", "ALERT", "CRIT", "ERROR", "WARN", "NOTICE", "INFO", "DEBUG" };
 
-class Log {
+class Log
+{
 public:
 
-    enum LogMethod {
-        CMethod = 0, CmdMethod, CmdWithHeadMethod, CmdOnlyWriteHeadMethod
-    };
+	enum LogMethod
+	{
+		CMethod = 0, CmdMethod, CmdWithHeadMethod, CmdOnlyWriteHeadMethod
+	};
 
-    static Log& instance() {
-        static Log _instance;
-        return _instance;
-    }  //Use static implement a instance.
+	static Log& instance()
+	{
+		static Log _instance;
+		return _instance;
+	}  //Use static implement a instance.
 
 //    void init(const char* dir, const char* fileName, uint32_t log_buf_size = ONE_LINE_LOG_LENGTH, uint32_t split_lines = SPLIT_LINES, uint32_t max_queue_size = 0);
-    void init(const char* dir, const char* fileName, uint32_t oneLineLogSize, uint32_t split_lines, double logTotalSize, Level logLevel, uint32_t max_queue_size = 0);
+	void init(const char* dir, const char* fileName, uint32_t oneLineLogSize, uint32_t split_lines, double logTotalSize, Level logLevel,
+			uint32_t max_queue_size = 0);
 
-    void writeLog(Level logLevel, AppModuleId moduleId, const char* format, ...);
+	void writeLog(Level logLevel, AppModuleId moduleId, const char* format, ...);
 
 private:
 
-    Log(); /*A private declaration for forbid inheriting*/
+	Log(); /*A private declaration for forbid inheriting*/
 
-    ~Log();
+	~Log();
 
-    void writeLogHead(char *logHead = NULL);
+	void writeLogHead(char *logHead = NULL);
 
-    size_t writeLogBody(Level logLevel, AppModuleId moduleId, char* des, size_t desLength, size_t offset, const char* format, va_list valst);
+	size_t writeLogBody(Level logLevel, AppModuleId moduleId, char* des, size_t desLength, size_t offset, const char* format, va_list valst);
 
-    bool moveLogs(const char* oldName, const char* newName, int32_t alreadyCompressFileAmount);
+	bool moveLogs(const char* oldName, const char* newName, int32_t alreadyCompressFileAmount);
 
-    void *async_write_log();
+	void *async_write_log();
 
-    //Must a static function, it would be called by pthread_create(), a function pointer point at it.
-    static void *flushLogThread(void* args) {
-        instance().async_write_log();
-    }
+	//Must a static function, it would be called by pthread_create(), a function pointer point at it.
+	static void *flushLogThread(void* args)
+	{
+		instance().async_write_log();
+	}
 
-    /* strings for printing message level */
-    const char* getLogLevelString(Level logLevel) const {
-        return logLevel < (static_cast<Level>(getArrayLen(logLevelString))) ? logLevelString[logLevel] : "Undefined";
-    }
+	/* strings for printing message level */
+	const char* getLogLevelString(Level logLevel) const
+	{
+		return logLevel < (static_cast<Level>(getArrayLen(logLevelString))) ? logLevelString[logLevel] : "Undefined";
+	}
 
-    /* strings for printing message level */
-    const char* getLogModuleString(AppModuleId logModule) const {
-        return logModule < (static_cast<AppModuleId>(getArrayLen(logModuleString))) ? logModuleString[logModule] : "Undefined";
-    }
+	/* strings for printing message level */
+	const char* getLogModuleString(AppModuleId logModule) const
+	{
+		return logModule < (static_cast<AppModuleId>(getArrayLen(logModuleString))) ? logModuleString[logModule] : "Undefined";
+	}
 
-    void logfilesControl(int32_t alreadyCompressFileAmount);
+	void logfilesControl(int32_t alreadyCompressFileAmount);
 
-    void flush() const;
+	void flush() const;
 
-    void logItself(LogMethod logMethod, Level logLevel, const char* format, ...);
+	void logItself(LogMethod logMethod, Level logLevel, const char* format, ...);
 
-    /* Attribute area */
-    pid_t pid;
+	/* Attribute area */
+	pid_t pid;
 
-    char pidName[NAME_LENGTH + 1];
+	char pidName[NAME_LENGTH + 1];
 
-    map<pid_t, int> mapThread;
+	map<pid_t, int> mapThread;
 
-    pthread_mutex_t *m_mutex;
+	pthread_mutex_t *m_mutex;
 
-    char dirPath[DIR_LENGTH + 1];
+	char dirPath[DIR_LENGTH + 1];
 
-    char logName[NAME_LENGTH + 1];   //default logName is "process name+'.log'"
+	char logName[NAME_LENGTH + 1];   //default logName is "process name+'.log'"
 
-    char logFullName[DIR_LENGTH + NAME_LENGTH + 2];
+	char logFullName[DIR_LENGTH + NAME_LENGTH + 2];
 
-    uint32_t splitLines;
+	uint32_t splitLines;
 
-    uint32_t oneLineLogLength;
+	uint32_t oneLineLogLength;
 
-    uint64_t counter;
+	uint64_t counter;
 
-    int32_t currentLogAmount;
+	int32_t currentLogAmount;
 
-    double logFilesTotalSize;
+	double logFilesTotalSize;
 
-    FILE* m_fp;
+	FILE* m_fp;
 
-    char* m_buf;
+	char* m_buf;
 
-    char logItselfBuf[BUF_SIZE];
+	char logItselfBuf[BUF_SIZE];
 
-    uint32_t logItselfLength;
+	uint32_t logItselfLength;
 
-    BlockQueue<string> *m_log_queue;
+	BlockQueue<string> *m_log_queue;
 
-    bool isAsync;
+	bool isAsync;
 
-    bool isStartup;
+	bool isStartup;
 
-    uint64_t lostNum;
+	uint64_t lostNum;
 
-    Level enableLogLevel;   // enable the log level and the upper level
+	Level enableLogLevel;   // enable the log level and the upper level
 };
 
 #define App_Log(logLevel, moduleId, ...)    Log::instance().writeLog(logLevel, moduleId, __VA_ARGS__)
--- Log/SingleProcessLog/test/LogMainTest.cpp
+++ Log/SingleProcessLog/test/LogMainTest.cpp
@@ -11,53 +11,58 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <sys/stat.h>
-//#include <pthread.h>
 
 #define THREAD_NUMBER   10
 #define COMPLEX_TEST     1
 
 #if COMPLEX_TEST
-void *f(void* args) {
-
-    for (int i = 0; i < 1000000; i++) {
-        App_Log(Notice, LastId, "Function:%s, %d, line=%d", __FUNCTION__, 1, __LINE__);
-        App_Log(Debug, LastId, "Function:%s, %d, line=%d", __FUNCTION__, 2, __LINE__);
-        App_Log(Notice, LastId, "Function:%s,%d, line=%d", __FUNCTION__, 3, __LINE__);
-    }
+void *f(void* args)
+{
+	for (int i = 0; i < 1000000; i++)
+	{
+		App_Log(Notice, LastId, "Function:%s, %d, line=%d", __FUNCTION__, 1, __LINE__);
+		App_Log(Debug, LastId, "Function:%s, %d, line=%d", __FUNCTION__, 2, __LINE__);
+		App_Log(Notice, LastId, "Function:%s,%d, line=%d", __FUNCTION__, 3, __LINE__);
+	}
 }
 #endif
 
-int main(int argc, char* argv[]) {
-    App_Log(Notice, LastId, "_______Function:%s, %d, line=%d", __FUNCTION__, 1, __LINE__);
+int main(int argc, char* argv[])
+{
+	App_Log(Notice, LastId, "_______Function:%s, %d, line=%d", __FUNCTION__, 1, __LINE__);
 
-    App_Log(Debug, LastId, "========Function:%s, %d, line=%d", __FUNCTION__, 2, __LINE__); //will not logging it, because the default log level is notice.
+	App_Log(Debug, LastId, "========Function:%s, %d, line=%d", __FUNCTION__, 2, __LINE__); //will not logging it, because the default log level is notice.
 //    Log::instance().init(".", "my-test.log", 1024, 500000, 2, Debug, 10000);
-    Log::instance().init(".", "my-test.log", 1024, 500000, 2, Debug, 0);
+	Log::instance().init(".", "my-test.log", 1024, 500000, 2, Debug, 0);
 
-    App_Log(Notice, LastId, "Function:%s, %d, line=%d", __FUNCTION__, 3, __LINE__);
+	App_Log(Notice, LastId, "Function:%s, %d, line=%d", __FUNCTION__, 3, __LINE__);
 
 #if COMPLEX_TEST
 
-    pthread_t id;
-    int ret;
+	pthread_t id;
+	int ret;
 
-    for (int i = 0; i < THREAD_NUMBER; i++) {
-        ret = pthread_create(&id, NULL, f, NULL);
-        if (0 != ret) {
-            App_Log(Error, LastId, "Function:%s, pthread_create error", __FUNCTION__);
-            exit(EXIT_FAILURE);
-        }
-    }
+	for (int i = 0; i < THREAD_NUMBER; i++)
+	{
+		ret = pthread_create(&id, NULL, f, NULL);
+		if (0 != ret)
+		{
+			App_Log(Error, LastId, "Function:%s, pthread_create error", __FUNCTION__);
+			exit(EXIT_FAILURE);
+		}
+	}
 
-    for (int i = 0; i < THREAD_NUMBER; i++) {
-        ret = pthread_join(id, NULL);
-        if (ret < 0) {
-            App_Log(Error, LastId, "Function:%s, pthread_join error", __FUNCTION__);
-            exit(EXIT_FAILURE);
-        }
-    }
+	for (int i = 0; i < THREAD_NUMBER; i++)
+	{
+		ret = pthread_join(id, NULL);
+		if (ret < 0)
+		{
+			App_Log(Error, LastId, "Function:%s, pthread_join error", __FUNCTION__);
+			exit(EXIT_FAILURE);
+		}
+	}
 #endif
 
-    return 0;
+	return 0;
 }
 
