16fe3245a070e82755614fe0798e71757164cdfd
piaoyimq <piaoyimq@qq.com>
2016-06-11 02:18:24 +0800

    fsm-transfer-table: modify test case

--- general/fsm/fsm-transfer-table/include/fsm-transfer.h
+++ general/fsm/fsm-transfer-table/include/fsm-transfer.h
@@ -20,16 +20,16 @@
 #define TRANSFER(Smobject, event) 		STATE_TRANSFER(Smobject, event, Sm, currentAtr, stateTransferTable, eventCallerTable)
 
 #define STATE_TRANSFER(arg1, arg2, arg3, arg4, fp1, fp2) \
-for (int i = 0; i < sizeof(fp1) / sizeof(fsm::StateTransfer<arg3::State, arg3::Event>); ++i)\
+for (int i = 0; i < sizeof(arg1.fp1) / sizeof(fsm::StateTransfer<arg3::State, arg3::Event>); ++i)\
 {\
-	if (arg1.arg4.state == fp1[i].currentState && arg2 == fp1[i].event)\
+	if (arg1.arg4.state == arg1.fp1[i].currentState && arg2 == arg1.fp1[i].event)\
 	{\
-		(arg1.*fp2[fp1[i].NextState])(arg1.arg4);\
+		(arg1.*arg1.fp2[arg1.fp1[i].NextState])(arg1.arg4);\
 		arg1.arg4.transferTimes++;\
 		std::cout << "transfer ok!\n";\
 		break;\
 	}\
-	if((sizeof(fp1) / sizeof(fsm::StateTransfer<arg3::State, arg3::Event>)) == i+1)\
+	if((sizeof(arg1.fp1) / sizeof(fsm::StateTransfer<arg3::State, arg3::Event>)) == i+1)\
 	{\
 		std::cout << "This event cannot transfer current state!!\n";\
 	}\
--- general/fsm/fsm-transfer-table/test/FsmTransferMainTest.cpp
+++ general/fsm/fsm-transfer-table/test/FsmTransferMainTest.cpp
@@ -25,61 +25,59 @@ public:
 		E_UNLOCK
 	};
 
+	typedef void (Sm::*pfToState)(fsm::CurrentAtr<State>&);
+	pfToState eventCallerTable[3];
+	fsm::StateTransfer<State, Event> stateTransferTable[4];
 	fsm::CurrentAtr<Sm::State> currentAtr ;
-		Sm()
-		{
-			currentAtr.state = S_CLOSED;
-			currentAtr.transferTimes = 0;
-		}
 
-		~Sm(){}
+	Sm()
+	{
+		currentAtr.state = S_CLOSED;
+		currentAtr.transferTimes = 0;
 
- void toOpen(fsm::CurrentAtr<State>& door)
-{
-	door.state = S_OPENED;
-	std::cout << "open the door!\n";
-}
+		//eventCallerTable  must initial in order by the State.
+		eventCallerTable[0] = &Sm::toOpen;
+		eventCallerTable[1] = &Sm::toClose;
+		eventCallerTable[2] = &Sm::toLock;
 
-void toClose(fsm::CurrentAtr<State>& door)
-{
-	door.state = S_CLOSED;
-	std::cout << "close the door!\n";
-}
+		//no order
+		stateTransferTable[0] =	{S_OPENED, E_CLOSE, S_CLOSED };
+		stateTransferTable[1] = {S_CLOSED, E_OPEN,  S_OPENED };
+		stateTransferTable[2] = {S_CLOSED, E_LOCK,  S_LOCKED };
+		stateTransferTable[3] = {S_LOCKED, E_UNLOCK,S_CLOSED };
+	}
 
-void toLock(fsm::CurrentAtr<State>& door)
-{
-	door.state = S_LOCKED;
-	std::cout << "lock the door!\n";
-}
+	~Sm(){}
 
-void printDoor(const fsm::CurrentAtr<State>& door)
-{
-	std::cout << "\n<<=============================\n";
-	std::string stateNote[] = { "opened", "closed", "locked" }; // 下标正好对应状态枚举值
-	std::cout << "the door's state is: " << stateNote[door.state] << std::endl;
-	std::cout << "the door transfer times is: " << door.transferTimes << std::endl;
+	 void toOpen(fsm::CurrentAtr<State>& door)
+	{
+		door.state = S_OPENED;
+		std::cout << "open the door!\n";
+	}
 
-	std::cout << "==============================>>\n";
-}
+	void toClose(fsm::CurrentAtr<State>& door)
+	{
+		door.state = S_CLOSED;
+		std::cout << "close the door!\n";
+	}
+
+	void toLock(fsm::CurrentAtr<State>& door)
+	{
+		door.state = S_LOCKED;
+		std::cout << "lock the door!\n";
+	}
+
+	void printDoor(const fsm::CurrentAtr<State>& door)
+	{
+		std::cout << "\n<<=============================\n";
+		std::string stateNote[] = { "opened", "closed", "locked" }; // 下标正好对应状态枚举值
+		std::cout << "the door's state is: " << stateNote[door.state] << std::endl;
+		std::cout << "the door transfer times is: " << door.transferTimes << std::endl;
+
+		std::cout << "==============================>>\n";
+	}
 };
 
-
-typedef void (Sm::*pfToState)(fsm::CurrentAtr<Sm::State>&);
-pfToState eventCallerTable[] =
-{
-		&Sm::toOpen,
-		&Sm::toClose,
-		&Sm::toLock
-}; //状态枚举值(State)对应下标
-
-
-fsm::StateTransfer<Sm::State, Sm::Event> stateTransferTable[]=
-{
-	  { Sm::S_OPENED, Sm::E_CLOSE, Sm::S_CLOSED },
-	  { Sm::S_CLOSED, Sm::E_OPEN,  Sm::S_OPENED },
-	  { Sm::S_CLOSED, Sm::E_LOCK,  Sm::S_LOCKED },
-	  { Sm::S_LOCKED, Sm::E_UNLOCK,Sm::S_CLOSED },
-};
 
 int main()
 {
