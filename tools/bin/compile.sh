0e2b5c57ef81ba54a25a7b37d587de803a3d8ae4
Weibo Zhu <weibo.zhu@ericsson.com>
2016-07-13 06:04:17 +0200

    compile.sh: modify some code
    Change-Id: If02c3ec0f95c33f39a22ebca7373ed7137d29b4a

--- tools/bin/compile.sh
+++ tools/bin/compile.sh
@@ -3,9 +3,9 @@
 #ulimit -a
 #ulimit -c unlimited
 set -x
-#INC="-I /opt/boost1.6/include"
-#LIB="-L /opt/boost1.6/lib -lpthread -lboost_serialization -lboost_system"
+INC_PATH="-I $BASE_PATH/boost_1_60_0/include"
+LIB_PATH="-L $BASE_PATH/boost_1_60_0/lib"
 LIB="-lpthread -lboost_serialization -lboost_system -lboost_filesystem"
+LIB_SO_PATH="-Wl,-rpath,$BASE_PATH/boost_1_60_0/lib/"
 
-
-g++ -g -std=c++0x "$@" $INC $LIB
+g++ -g -std=c++0x "$@" $INC_PATH $LIB_PATH $LIB $LIB_SO_PATH
