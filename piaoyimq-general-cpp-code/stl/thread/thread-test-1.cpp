a9d6b756d6eaff92e2a63358cdb7653d97fd3ee9
Weibo Zhu <weibo.zhu@ericsson.com>
2017-08-08 05:22:36 +0200

    thread
    Change-Id: Iebc2bbfb983807b59901ef8185e1be555f0c417e

---
+++ piaoyimq-general-cpp-code/stl/thread/thread-test-1.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <utility>
+#include <thread>
+#include <chrono>
+#include <functional>
+#include <atomic>
+
+void f1(int n)
+{
+	std::cout <<"f1, "<< "std::this_thread::get_id()="<<std::this_thread::get_id() << std::endl;
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Thread 1 executing\n";
+        ++n;
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+}
+
+void f2(int& n)
+{
+	std::cout <<"f2, " <<"std::this_thread::get_id()="<<std::this_thread::get_id() << std::endl;
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Thread 2 executing\n";
+        ++n;
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+}
+
+int main()
+{
+    int n = 0;
+    std::thread t1; // t1 is not a thread
+    std::cout << std::boolalpha;
+    std::cout << "t1.joinable=" << t1.joinable() << '\n';
+
+    std::thread t2(f1, n + 1); // pass by value
+    std::cout << "t2.joinable=" << t2.joinable() << '\n';
+
+    std::thread t3(f2, std::ref(n)); // pass by reference
+    std::cout << "t3.joinable=" << t3.joinable() << '\n';
+
+    std::thread::id t3_id_1 = t3.get_id();
+
+    std::thread t4(std::move(t3)); // t4 is now running f2(). t3 is no longer a thread
+    std::cout << "t3.joinable=" << t3.joinable() << '\n';
+    std::cout << "t4.joinable=" << t4.joinable() << '\n';
+
+    std::thread::id t1_id = t1.get_id();
+    std::thread::id t2_id = t2.get_id();
+    std::thread::id t3_id = t3.get_id();
+    std::thread::id t4_id = t4.get_id();
+
+    std::cout<< "t1_id=" <<t1_id << std::endl;
+    std::cout<< "t2_id=" <<t2_id << std::endl;
+    std::cout<< "t3_id_1=" <<t3_id_1 << std::endl;
+    std::cout<< "t3_id=" <<t3_id << std::endl;
+    std::cout<< "t4_id=" <<t4_id << std::endl;
+
+    std::cout << "std::this_thread::get_id()="<<std::this_thread::get_id() << std::endl;
+    t2.join();
+    t4.join();
+    std::cout << "Final value of n is " << n << '\n';
+
+    std::cout << "std::thread::id()="<<std::thread::id()<< std::endl;
+}
