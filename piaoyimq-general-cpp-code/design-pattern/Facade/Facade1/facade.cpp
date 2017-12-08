9e9c80e2b6d69c1f6857e9b48e03c5e9ca03dc4a
piaoyimq <piaoyimq@qq.com>
2017-12-08 22:39:51 +0800

    Change font in eclipse
    Change-Id: I0ef2f23e8c3c02ad6a6c81d3d8e1830334fea876

--- piaoyimq-general-cpp-code/design-pattern/Facade/Facade1/facade.cpp
+++ piaoyimq-general-cpp-code/design-pattern/Facade/Facade1/facade.cpp
@@ -9,39 +9,47 @@
 #include <iostream>
 using namespace std;
 
-
-Subsystem1::Subsystem1() {
+Subsystem1::Subsystem1()
+{
 }
 
-Subsystem1::~Subsystem1() {
+Subsystem1::~Subsystem1()
+{
 }
 
-void Subsystem1::Operation() {
-	cout << "Subsystem1 operation.." << endl;
+void Subsystem1::Operation()
+{
+    cout << "Subsystem1 operation.." << endl;
 }
 
-Subsystem2::Subsystem2() {
+Subsystem2::Subsystem2()
+{
 }
 
-Subsystem2::~Subsystem2() {
+Subsystem2::~Subsystem2()
+{
 }
 
-void Subsystem2::Operation() {
-	cout << "Subsystem2 operation.." << endl;
+void Subsystem2::Operation()
+{
+    cout << "Subsystem2 operation.." << endl;
 }
 
-Facade::Facade() {
-	_subs1 = new Subsystem1();
-	_subs2 = new Subsystem2();
+Facade::Facade()
+{
+    _subs1 = new Subsystem1();
+    _subs2 = new Subsystem2();
 }
 
-Facade::~Facade() {
-	delete _subs1;
-	delete _subs2;
+Facade::~Facade()
+{
+    delete _subs1;
+    delete _subs2;
 }
 
-void Facade::OperationWrapper() {
-	_subs1->Operation();
-	_subs2->Operation();
+void Facade::OperationWrapper()
+{
+    _subs1->Operation();
+    _subs2->Operation();
 }
 
--- piaoyimq-general-cpp-code/design-pattern/Facade/Facade1/main.cpp
+++ piaoyimq-general-cpp-code/design-pattern/Facade/Facade1/main.cpp
@@ -9,11 +9,11 @@
 #include <iostream>
 using namespace std;
 
+int main(int argc, char* argv[])
+{
+    Facade* f = new Facade();
+    f->OperationWrapper();
 
-int main(int argc, char* argv[]) {
-	Facade* f = new Facade();
-	f->OperationWrapper();
-
-	return 0;
+    return 0;
 }
 
