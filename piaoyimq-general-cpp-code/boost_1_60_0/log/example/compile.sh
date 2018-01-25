64073ff65e64d57b2224ab158761a175decd79a6
piaoyimq <piaoyimq@qq.com>
2018-01-25 08:43:11 +0000

    Update compile.sh

--- piaoyimq-general-cpp-code/boost_1_60_0/log/example/compile.sh
+++ piaoyimq-general-cpp-code/boost_1_60_0/log/example/compile.sh
@@ -1,3 +1,3 @@
 
 set -x
-g++ -g -std=c++0x $1 -I $BASE_PATH/boost_1_60_0/include -DBOOST_LOG_DYN_LINK -DBOOST_LOG_USE_NATIVE_SYSLOG -L $BASE_PATH/boost_1_60_0/lib -lpthread -lboost_system -lboost_log_setup  -lboost_log  -lboost_thread -lboost_locale -Wl,-rpath,$BASE_PATH/boost_1_60_0/lib/
+g++ -g -std=c++0x $1 -I $BASE_PATH/CppSpace/staging/Linux_x86_64/usr/local/include -DBOOST_LOG_DYN_LINK -DBOOST_LOG_USE_NATIVE_SYSLOG -L $BASE_PATH/CppSpace/staging/Linux_x86_64/usr/local/lib -lpthread -lboost_system -lboost_log_setup  -lboost_log  -lboost_thread -lboost_locale -Wl,-rpath,$BASE_PATH/CppSpace/staging/Linux_x86_64/usr/local/lib/
