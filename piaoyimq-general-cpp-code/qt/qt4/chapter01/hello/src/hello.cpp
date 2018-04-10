d423b0beb5dbd0d7f06616d3229aff01c606fd9a
piaoyimq <piaoyimq@qq.com>
2018-04-10 15:16:51 +0800

    delete chinese file name

--- piaoyimq-general-cpp-code/qt/qt4/chapter01/hello/src/hello.cpp
+++ piaoyimq-general-cpp-code/qt/qt4/chapter01/hello/src/hello.cpp
@@ -29,8 +29,8 @@ int main(int argc, char* argv[])
 	QTextCodec::setCodecForTr(QTextCodec::codecForName("gb18030"));
 	QWidget* pWidget = new QWidget;
 	QLabel label(pWidget);
-	label.setText(QObject::tr("同一个世界，同一个梦想！"));
-	QPushButton* btn = new QPushButton(QObject::tr("关闭"), pWidget);
+	label.setText(QObject::tr("Test"));
+	QPushButton* btn = new QPushButton(QObject::tr("button"), pWidget);
 	QVBoxLayout* layout = new QVBoxLayout;
 	layout->addWidget(&label);
 	layout->addWidget(btn);
