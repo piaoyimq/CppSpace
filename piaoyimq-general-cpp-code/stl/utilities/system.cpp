1c0d8b62a552e30479b2471c56b62c9fc5f4325e
piaoyimq <piaoyimq@qq.com>
2018-05-10 17:16:27 +0800

    add poco-1.9.0

--- piaoyimq-general-cpp-code/stl/utilities/system.cpp
+++ piaoyimq-general-cpp-code/stl/utilities/system.cpp
@@ -7,9 +7,9 @@ int runSystemCommand(const std::string& command)
 {
 
     int result = -1;
-    if (std::system(NULL))
+    if (system(NULL))
     {
-        result = std::system(command.c_str()); // execute the command
+        result = system(command.c_str()); // execute the command
     }
     else
     {
@@ -20,12 +20,61 @@ int runSystemCommand(const std::string& command)
     return WEXITSTATUS(result);
 }
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+bool runSystemCommand2(const char* cmd)
+{
+    pid_t status;
+
+
+    status = system(cmd);
+
+    if (-1 == status)
+    {
+        printf("system error!");
+        return false;
+    }
+    else
+    {
+        printf("exit status value = [0x%x]\n", status);
+
+        if (WIFEXITED(status))
+        {
+            if (0 == WEXITSTATUS(status))
+            {
+                printf("run shell script successfully.\n");
+                return true;
+            }
+            else
+            {
+                printf("run shell script fail, script exit code: %d\n", WEXITSTATUS(status));
+                return false;
+            }
+        }
+        else
+        {
+            printf("exit status = [%d]\n", WEXITSTATUS(status));
+            return false;
+        }
+    }
+
+    return 0;
+}
 
 int main()
 {
-	int a = runSystemCommand("ls");
+	int a = runSystemCommand("lss");
 	if(a> 0)
 	{
-		std::cout << "Execute the command successuflly" << std::endll;
+		std::cout << "Execute the command successuflly" << std::endl;
+	}
+
+	bool ret = runSystemCommand2("lss");
+	if(!ret)
+	{
+	    std::cout << "Execute the command failed" << std::endl;
 	}
 }
